e77c58eca5438d66c2124c30690f5f252ccd6e7e
ewa.miazga <ewa.miazga@redge.com>
2023-02-24 10:42:08 +0100

    Add tests to class GreetingsActivityTest

--- app/src/androidTest/java/com/github/miazga/bootcampapp/GreetingActivityTest.java
+++ app/src/androidTest/java/com/github/miazga/bootcampapp/GreetingActivityTest.java
@@ -1,6 +1,12 @@
 package com.github.miazga.bootcampapp;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Intent;
+import android.widget.TextView;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
@@ -15,13 +21,28 @@ import java.nio.channels.InterruptedByTimeoutException;
 @RunWith(AndroidJUnit4.class)
 public class GreetingActivityTest {
 
-    /*@Test public void testIntent() {
+
+    @Test
+    public void testIntent() {
         Intents.init();
-        ApplicationProvider.getApplicationContext();
-        Intent i = new Intent();
+        Intent i = new Intent(ApplicationProvider.getApplicationContext(), Greeting_Activity.class);
         ActivityScenario.launch(i).close();
+
+        onView(withId(R.id.textView));
+        Intents.release();
+    }
+
+    /*@Test
+    public void testIntentMatch() {
+        Intents.init();
+        Intent i = new Intent(ApplicationProvider.getApplicationContext(), Greeting_Activity.class);
+        ActivityScenario.launch(i).close();
+
+        onView(withId(R.id.textView))
+                .check(matches(withText("Greetings Enter your name")));
         Intents.release();
     }*/
+
     @Test
     public void newTest() {
 
